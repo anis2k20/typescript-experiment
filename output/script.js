7f62fdeaa94b2f9a6993f6d95868986deee70dd2
Anis <anis2k20@gmail.com>
2024-11-02 21:32:43 +0600

    initial

---
+++ output/script.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello world!");
+const country = "Bangladesh is my country";
+console.log(country);
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+console.log("Hello world!");
+
+const country = "Bangladesh";
+
+console.log(country);
