0df143620c08423367f2c6f9015ab5f132c67bc7
Anis <anis2k20@gmail.com>
2024-11-04 21:56:58 +0600

    type practice

--- output/script.js
+++ output/script.js
@@ -1,4 +1,29 @@
 "use strict";
 console.log("Hello world!");
-const country = "Bangladesh is my country";
+const country = "Bangladesh";
 console.log(country);
+let playerName = "Anis";
+let age = 34;
+console.log(playerName);
+playerName = "sumon";
+age = 23;
+let player;
+player = "anis";
+player = 25;
+console.log(player);
+function mul(a, b) {
+    return a * b;
+}
+console.log(mul(4, 3));
+let fruits = ["apple", 3, true, {}];
+fruits.push(2);
+fruits.push({
+    name: "mango",
+});
+console.log(fruits);
+let person = {
+    name: "anis",
+    age: 30,
+    isMale: true,
+};
+person.name = "shakil";
--- src/script.ts
+++ src/script.ts
@@ -3,3 +3,39 @@ console.log("Hello world!");
 const country = "Bangladesh";
 
 console.log(country);
+
+let playerName = "Anis";
+let age = 34;
+console.log(playerName);
+playerName = "sumon";
+age = 23;
+
+let player;
+
+player = "anis";
+
+player = 25;
+
+console.log(player);
+
+function mul(a: number, b: number) {
+  return a * b;
+}
+
+console.log(mul(4, 3));
+
+let fruits = ["apple", 3, true, {}];
+fruits.push(2);
+fruits.push({
+  name: "mango",
+});
+
+console.log(fruits);
+
+let person = {
+  name: "anis",
+  age: 30,
+  isMale: true,
+};
+
+person.name = "shakil";
